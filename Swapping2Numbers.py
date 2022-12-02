60e7f5d5b1997e27ebd71fd22440f15bc3a8aa4c
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-12-02 02:07:39 -0300

    :construction:

---
+++ Swapping2Numbers.py
@@ -0,0 +1,20 @@
+
+# num1=10
+# num2=20
+
+num1=input("Enter num1 value")
+num2=input("Enter num2 value")
+
+print("Value of num1 before swapping:", num1)
+print("Value of num2 before swapping:", num2)
+
+#Approach1 temporaly variable
+# temp=num1
+# num1=num2
+# num2=temp
+
+num1,num2=num2,num1
+
+print("Value of num1 before swapping:", num1)
+print("Value of num2 before swapping:", num2)
+
