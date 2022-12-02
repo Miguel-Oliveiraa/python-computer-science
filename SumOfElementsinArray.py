861ef96998520ab3e15901a34a0a2e686b71063b
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-12-02 02:53:34 -0300

    :construction:

--- SumOfElementsinArray.py
+++ SumOfElementsinArray.py
@@ -1,6 +1,12 @@
 arr = [1,2,3,4,5]
+result = 0
 
+# By me
+for i in range(0, len(arr)):
+    result += arr[i]
+print(result)
+
+# Function sum
 print(sum(arr))
-
 print(sum(arr, 10))
 print(sum(arr, -10))
